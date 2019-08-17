016e6217d35a6a339dc44c7f7650da44f5d26793
赖荣斌1 <A-315@g2.local>
2019-08-17 10:39:40 +0800

    用户界面 - 使用优惠券下单

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -141,8 +141,10 @@
         var req = {
           address_id: $('#order-form').find('select[name=address]').val(),
           items: [],
-          remark: $('#order-form').find('textarea[name=remark]').val()
+          remark: $('#order-form').find('textarea[name=remark]').val(),
+          coupon_code: $('input[name=coupon_code]').val(),  // 优惠码
         };
+        console.log(req);
         // 遍历 <table> 标签内所有带有data-id属性的 tr 标签
         $('table tr[data-id]').each(function() {
           // 获取当前行的单选框
