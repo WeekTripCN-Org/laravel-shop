7a149595d3e2e4a283df724c35b01a32f5342129
赖荣斌1 <A-315@g2.local>
2019-08-20 10:39:52 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -3,141 +3,36 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Product;
-use App\Http\Controllers\Controller;
-use App\Models\Category;
-use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
-use Encore\Admin\Layout\Content;
-use Encore\Admin\Show;
 
-class ProductsController extends Controller
+class ProductsController extends CommonProductsController
 {
-    use HasResourceActions;
+   public function getProductType()
+   {
+       return Product::TYPE_NORMAL;
+   }
 
-    /**
-     * Index interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('商品列表')
-            ->description('description')
-            ->body($this->grid());
-    }
+   protected function customGrid(Grid $grid)
+   {
+       $grid->model()->with(['category']);
+       $grid->id('ID')->sortable();
+       $grid->title('商品名称');
 
-    /**
-     * Edit interface.
-     *
-     * @param mixed $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('编辑商品')
-            ->description('description')
-            ->body($this->form()->edit($id));
-    }
+       // 支持用符号 . 来展示关联关系的字段
+       $grid->column('category.name', '类目');
 
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('创建商品')
-            ->description('description')
-            ->body($this->form());
-    }
+       $grid->on_sale('已上架')->display(function($value) {
+           return $value ? '是' : '否';
+       });
+       $grid->price('价格');
+       $grid->rating('评分');
+       $grid->sold_count('销量');
+       $grid->review_count('评论数');
+   }
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
-    {
-        $grid = new Grid(new Product);
-
-        // 使用 with 来预加载商品类目数据，减少 SQL 查询
-        $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
-
-        $grid->id('Id')->sortable();
-        $grid->title('商品名称');
-
-        // 支持用符号 . 来展示关联关系的字段
-        $grid->column('category.name', '类目');
-
-        $grid->on_sale('已上架')->display(function($value) {
-            return $value ? '是' : '否';
-        });
-        $grid->price('价格');
-        $grid->rating('评分');
-        $grid->sold_count('销量');
-        $grid->review_count('评论数');
-
-        $grid->actions(function($actions) {
-            $actions->disableView();
-            $actions->disableDelete();
-        });
-        
-        $grid->tools(function ($tools) {
-            $tools->batch(function($batch) {
-                $batch->disableDelete();
-            });
-        });
-
-        return $grid;
-    }
-
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new Product);
-        // 在表单中添加一个名为 type，值为 Product::TYPE_NORMAL 的隐藏字段
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
-
-        $form->text('title', '商品名称')->rules('required');
-
-        // 添加一个类目字段，与之前类目管理类似，使用 Ajax 的方式来搜索添加
-        $form->select('category_id', '类目')->options(function($id) {
-            $category = Category::find($id);
-            if ($category) {
-                return [$category->id => $category->full_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-        
-        $form->image('image', '封面图片')->rules('required|image');
-
-        $form->editor('description', '商品描述')->rules('required');
-
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
-
-        // 添加一对多的关联模型
-        $form->hasMany('skus', 'SKU 列表', function(Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-        });
-
-        // 定义事件回调，当模型即将保存时会触发这个回调
-        // 在保存商品之前拿到所有 SKU 中最低的价格作为商品的价格，然后通过 $form->model()->price 存入到商品模型中
-        $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-        });
-        return $form;
-    }
+   protected function customForm(Form $form)
+   {
+       // 普通商品没有额外的字段，因此这里不需要任何代码
+   }
 }
