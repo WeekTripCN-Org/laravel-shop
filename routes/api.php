83ad8dcc8aa4c046eecf794f03689ad3ae0cb459
WeekTrip <weektrip@foxmail.com>
2019-09-12 11:35:35 +0800

    删除测试的 api

--- routes/api.php
+++ routes/api.php
@@ -22,14 +22,5 @@ $api->version('v1', [
         $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
     });
     
-    
-    // $api->post('login', 'AuthController@login');
-    // $api->post('logout', 'AuthController@logout');
-    // $api->post('refresh', 'AuthController@refresh');
-    // $api->post('me', 'AuthController@me');
-    // $api->post('register', 'AuthController@register');
-
-    // $api->post('getProductsByCategoryId', 'ProductsController@getProductsByCategoryId');
-    
 });
 
